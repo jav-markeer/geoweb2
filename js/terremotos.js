14f28e258d782912cfb9b5dca19c24db2a32a484
Javier Tella Guerrero <javiertg4@TTC.CAT>
2020-01-15 20:36:55 +0100

    terremotos

--- js/terremotos.js
+++ js/terremotos.js
@@ -105,26 +105,26 @@ function terremotosGeonamesToGeoJSON(respuestaGeonames){
     function addPopupToMap(nombreCapa) {
 
         map.on('click', nombreCapa, function (e) {
-
-            var text = "";
-            //console.info(e);
-            for (key in e.features[0].properties) {
-
-                text += "<b>" + key + "</b>:" + e.features[0].properties[key] + "<br>";
-             }
-            new mapboxgl.Popup()
+      
+          var text = "";
+          //console.info(e);
+          for (key in e.features[0].properties) {
+      
+            text += "<b>" + key + "</b>:" + e.features[0].properties[key] + "<br>";
+          }
+          new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(text)
             .addTo(map);
-
+      
         });
-
+      
         map.on('mouseenter', nombreCapa, function () {
-            map.getCanvas().style.cursor = 'pointer';
+          map.getCanvas().style.cursor = 'pointer';
         });
-
+      
         map.on('mouseleave', nombreCapa, function () {
-            map.getCanvas().style.cursor = '';
+          map.getCanvas().style.cursor = '';
         });
-
-}
+      
+      }
