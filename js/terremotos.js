793574551ab92cf7e7a319cd3889456175106f88
Javier Tella Guerrero <javiertg4@TTC.CAT>
2020-01-15 20:46:05 +0100

    terremotos

--- js/terremotos.js
+++ js/terremotos.js
@@ -102,29 +102,3 @@ function terremotosGeonamesToGeoJSON(respuestaGeonames){
     
     } // fin funcion
     
-    function addPopupToMap(nombreCapa) {
-
-        map.on('click', nombreCapa, function (e) {
-      
-          var text = "";
-          //console.info(e);
-          for (key in e.features[0].properties) {
-      
-            text += "<b>" + key + "</b>:" + e.features[0].properties[key] + "<br>";
-          }
-          new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML(text)
-            .addTo(map);
-      
-        });
-      
-        map.on('mouseenter', nombreCapa, function () {
-          map.getCanvas().style.cursor = 'pointer';
-        });
-      
-        map.on('mouseleave', nombreCapa, function () {
-          map.getCanvas().style.cursor = '';
-        });
-      
-    }
